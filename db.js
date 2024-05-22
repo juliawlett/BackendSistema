412cc225af2bc48280a4e06a6b8db80f570b613f
juliawlett <julialeticia100@gmail.com>
2024-05-22 08:25:53 -0400

    uptade

--- db.js
+++ db.js
@@ -1,16 +1,11 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const dbHost = process.env.DB_HOST;
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
-const dbName = process.env.DB_NAME;
-
 const connection = mysql.createConnection({
-  host: dbHost,
-  user: dbUser,
-  password: dbPassword,
-  database: dbName,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
--- index.js
+++ index.js
@@ -1,32 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors'); // Importe o pacote 'cors'
+const cors = require('cors');
 const db = require('./db');
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://juliafullstack.site');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-
-app.use(cors());
+app.use(cors({ origin: 'https://juliafullstack.site' }));
 app.use(bodyParser.json());
 
 app.post('/cadastrar', (req, res) => {
-    const { primeiroNome, segundoNome, emailUsuario, senhaHash } = req.body;
+  const { primeiroNome, segundoNome, emailUsuario, senhaHash } = req.body;
 
-    db.query('INSERT INTO Usuario (primeiroNome, segundoNome, emailUsuario, senhaHash) VALUES (?, ?, ?, ?)', [primeiroNome, segundoNome, emailUsuario, senhaHash], (err, result) => {
-        if (err) {
-            console.error('Erro ao cadastrar usuário:', err);
-            return res.status(500).json({ error: 'Erro ao cadastrar usuário.' });
-        }
+  db.query(
+    'INSERT INTO Usuario (primeiroNome, segundoNome, emailUsuario, senhaHash) VALUES (?, ?, ?, ?)',
+    [primeiroNome, segundoNome, emailUsuario, senhaHash],
+    (err, result) => {
+      if (err) {
+        console.error('Erro ao cadastrar usuário:', err);
+        return res.status(500).json({ error: 'Erro ao cadastrar usuário.' });
+      }
 
-        console.log('Usuário cadastrado com sucesso:', result);
-        res.status(200).json({ message: 'Usuário cadastrado com sucesso!' });
-    });
+      console.log('Usuário cadastrado com sucesso:', result);
+      res.status(200).json({ message: 'Usuário cadastrado com sucesso!' });
+    }
+  );
 });
 
 module.exports = app;
