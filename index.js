47f37d2021b1f6a01f61c7c593491c2865a91d40
juliawlett <julialeticia100@gmail.com>
2024-05-22 00:32:04 -0400

    uptade

--- index.js
+++ index.js
@@ -10,25 +10,23 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
-  });
+});
 
 app.use(cors());
-app.use(cors({ origin: 'https://juliafullstack.site' }));
-
 app.use(bodyParser.json());
 
 app.post('/cadastrar', (req, res) => {
     const { primeiroNome, segundoNome, emailUsuario, senhaHash } = req.body;
-  
+
     db.query('INSERT INTO Usuario (primeiroNome, segundoNome, emailUsuario, senhaHash) VALUES (?, ?, ?, ?)', [primeiroNome, segundoNome, emailUsuario, senhaHash], (err, result) => {
-      if (err) {
-        console.error('Erro ao cadastrar usuário:', err);
-        return res.status(500).json({ error: 'Erro ao cadastrar usuário.' });
-      }
-  
-      console.log('Usuário cadastrado com sucesso:', result);
-      res.status(200).json({ message: 'Usuário cadastrado com sucesso!' });
+        if (err) {
+            console.error('Erro ao cadastrar usuário:', err);
+            return res.status(500).json({ error: 'Erro ao cadastrar usuário.' });
+        }
+
+        console.log('Usuário cadastrado com sucesso:', result);
+        res.status(200).json({ message: 'Usuário cadastrado com sucesso!' });
     });
-  });
+});
 
 module.exports = app;
