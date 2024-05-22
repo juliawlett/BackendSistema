56116b8a63cd150a5f96dbf5de067e2c6594c63f
juliawlett <julialeticia100@gmail.com>
2024-05-21 23:35:51 -0400

    first commit

---
+++ db.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err.stack);
+    return;
+  }
+  console.log('Conectado ao banco de dados como ID', connection.threadId);
+});
+
+module.exports = connection;
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors'); // Importe o pacote 'cors'
+const db = require('./db');
+
+const app = express();
+const port = 3000;
+
+// Use o middleware cors
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.post('/cadastrar', (req, res) => {
+    const { primeiroNome, segundoNome, emailUsuario, senhaHash } = req.body;
+  
+    db.query('INSERT INTO Usuario (primeiroNome, segundoNome, emailUsuario, senhaHash) VALUES (?, ?, ?, ?)', [primeiroNome, segundoNome, emailUsuario, senhaHash], (err, result) => {
+      if (err) {
+        console.error('Erro ao cadastrar usuário:', err);
+        return res.status(500).json({ error: 'Erro ao cadastrar usuário.' });
+      }
+  
+      console.log('Usuário cadastrado com sucesso:', result);
+      res.status(200).json({ message: 'Usuário cadastrado com sucesso!' });
+    });
+  });
+  
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
